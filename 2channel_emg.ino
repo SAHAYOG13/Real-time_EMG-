7ff3e71b57b4f9dd610570b6f29183bdb7c2f6db
Ananthu V <57823897+ananthuvng@users.noreply.github.com>
2023-05-01 15:37:06 +0530

    arduino code for 2 channel emg acquasition

---
+++ 2channel_emg.ino
@@ -0,0 +1,105 @@
+#define SAMPLE_RATE 500
+#define BAUD_RATE 115200
+#define BUFFER_SIZE 128
+const int emgPin1 = A0;
+const int emgPin2 = A2;
+int emg1=0,emg2=0;
+
+int circular_buffer_1[BUFFER_SIZE],circular_buffer_2[BUFFER_SIZE];
+int data_index_1, sum_1,data_index_2, sum_2;
+void setup() {
+  Serial.begin(115200);
+  pinMode(emgPin1, INPUT);
+  pinMode(emgPin2, INPUT);
+
+}
+void loop() {    // Calculate elapsed time
+  static unsigned long past = 0;
+  unsigned long present = micros();
+  unsigned long interval = present - past;
+  past = present;
+  static long timer = 0;
+  timer -= interval;
+  if(timer < 0) {
+    timer += 1000000 / SAMPLE_RATE; // get the current time
+    int emg1 = analogRead(emgPin1);
+    int emgValue1 = EMGFilter(emg1);
+    int emg2 = analogRead(emgPin2);
+    int emgValue2 = EMGFilter1(emg2);
+
+ 
+   
+
+  Serial.print(emgValue1);
+   Serial.print(",");
+   Serial.println(emgValue2);
+
+}
+}
+
+float EMGFilter(float input)
+{
+  float output = input;
+  {
+    static float z1, z2; // filter section state
+    float x = output - 0.05159732*z1 - 0.36347401*z2;
+    output = 0.01856301*x + 0.03712602*z1 + 0.01856301*z2;
+    z2 = z1;
+    z1 = x;
+  }
+  {
+    static float z1, z2; // filter section state
+    float x = output - -0.53945795*z1 - 0.39764934*z2;
+    output = 1.00000000*x + -2.00000000*z1 + 1.00000000*z2;
+    z2 = z1;
+    z1 = x;
+  }
+  {
+    static float z1, z2; // filter section state
+    float x = output - 0.47319594*z1 - 0.70744137*z2;
+    output = 1.00000000*x + 2.00000000*z1 + 1.00000000*z2;
+    z2 = z1;
+    z1 = x;
+  }
+  {
+    static float z1, z2; // filter section state
+    float x = output - -1.00211112*z1 - 0.74520226*z2;
+    output = 1.00000000*x + -2.00000000*z1 + 1.00000000*z2;
+    z2 = z1;
+    z1 = x;
+  }
+  return output;
+}
+float EMGFilter1(float input)
+{
+  float output = input;
+  {
+    static float z1, z2; // filter section state
+    float x = output - 0.05159732*z1 - 0.36347401*z2;
+    output = 0.01856301*x + 0.03712602*z1 + 0.01856301*z2;
+    z2 = z1;
+    z1 = x;
+  }
+  {
+    static float z1, z2; // filter section state
+    float x = output - -0.53945795*z1 - 0.39764934*z2;
+    output = 1.00000000*x + -2.00000000*z1 + 1.00000000*z2;
+    z2 = z1;
+    z1 = x;
+  }
+  {
+    static float z1, z2; // filter section state
+    float x = output - 0.47319594*z1 - 0.70744137*z2;
+    output = 1.00000000*x + 2.00000000*z1 + 1.00000000*z2;
+    z2 = z1;
+    z1 = x;
+  }
+  {
+    static float z1, z2; // filter section state
+    float x = output - -1.00211112*z1 - 0.74520226*z2;
+    output = 1.00000000*x + -2.00000000*z1 + 1.00000000*z2;
+    z2 = z1;
+    z1 = x;
+  }
+  return output;
+}
